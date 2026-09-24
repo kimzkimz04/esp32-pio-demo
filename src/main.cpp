ce76f6b6809606c51c91aeaf8d539a33aa239851
kimzkimz04 <jcvivo35@gmail.com>
2026-09-24 15:01:55 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #define LED_PIN 2
 
 unsigned long lastBlink = 0;
-const unsigned long BLINK_INTERVAL = 250;
+const unsigned long BLINK_INTERVAL = 500;
 bool ledState = false;
 int blinkCount = 0;
 
